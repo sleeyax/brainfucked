e6b579388e4a2eb96fa9c92da6d630a738bc792a
Sleeyax <yourd3veloper@gmail.com>
2019-08-04 00:00:14 +0200

    Nest loops

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,11 @@ import 'package:brainfucked/parser/parser.dart';
 import 'package:brainfucked/tokenizer/tokenizer.dart';
 
 main(List<String> arguments) {
-  BrainfuckTokenizer tokenizer = new BrainfuckTokenizer("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
-  BrainfuckParser parser = new BrainfuckParser(tokenizer);
-  print(parser.parse());
+  // tokenize input
+  BrainfuckTokenizer tokenizer = BrainfuckTokenizer("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
+  var tokens = tokenizer.tokenize();
+
+  // parse tokens / create AST
+  BrainfuckParser parser = BrainfuckParser();
+  print(parser.parse(tokens.iterator));
 }
